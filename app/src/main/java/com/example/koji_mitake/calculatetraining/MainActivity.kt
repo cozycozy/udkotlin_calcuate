1c83f4340cd5dda6d57728a351a63c29defc469c
koji_mitake <koji_mitake@metaps.com>
2017-11-21 21:09:44 +0900

    82までDone

--- app/src/main/java/com/example/koji_mitake/calculatetraining/MainActivity.kt
+++ app/src/main/java/com/example/koji_mitake/calculatetraining/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.example.koji_mitake.calculatetraining
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+//        var arrayAdapter : ArrayAdapter<Int> = ArrayAdapter<Int>(this,android.R.layout.simple_spinner_item)
+//        arrayAdapter.add(10)
+//        arrayAdapter.add(20)
+//        arrayAdapter.add(30)
+
+        //arrayAdapterに値を入れる
+        val arrayAdapter = ArrayAdapter.createFromResource(this,R.array.nuber_of_question,android.R.layout.simple_spinner_item)
+
+        // Spinnerに値を入れる
+        spinner.adapter = arrayAdapter
+
+        button.setOnClickListener {
+
+            //Spinnerの値を取る
+        val selectedItem : Int = spinner.selectedItem.toString().toInt()
+
+            //インテントを作り、値を入れ、画面を開く
+            val intent = Intent(this@MainActivity,TestActivity::class.java)
+            intent.putExtra("selectecItem",selectedItem)
+            startActivity(intent)
+        }
+
+
     }
 }
--- app/src/main/java/com/example/koji_mitake/calculatetraining/TestActivity.kt
+++ app/src/main/java/com/example/koji_mitake/calculatetraining/TestActivity.kt
@@ -2,11 +2,67 @@ package com.example.koji_mitake.calculatetraining
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_test.*
 
-class TestActivity : AppCompatActivity() {
+class TestActivity : AppCompatActivity (), View.OnClickListener {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+
+        //前の画面の問題数を表示
+        val bundle : Bundle = intent.extras
+        val selectedItem : Int = bundle.getInt("selectecItem")
+
+        //画面に表示
+        textViewRemaining.text = selectedItem.toString()
+
+        //クリックリスナーの登録
+        buttonAnswer.setOnClickListener {
+            answerCheck()
+        }
+
+        buttonBack.setOnClickListener{
+
+        }
+
+        button0.setOnClickListener(this)
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+        button5.setOnClickListener(this)
+        button6.setOnClickListener(this)
+        button7.setOnClickListener(this)
+        button8.setOnClickListener(this)
+        button9.setOnClickListener(this)
+        buttonClear.setOnClickListener(this)
+        buttonMinus.setOnClickListener(this)
+
+        //一問目を出す
+        question()
+
     }
+
+    override fun onClick(p0: View?) {
+
+    }
+
+    // 答え合わせが押されたら
+    private fun answerCheck() {
+
+    }
+
+    private fun question() {
+
+    }
+
+
+    // 電卓が押された場合
+
+
+
 }
