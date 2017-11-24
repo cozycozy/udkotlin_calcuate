520c2d0af7bfa096f1852a2597ee400b1740ea74
koji_mitake <koji_mitake@metaps.com>
2017-11-24 11:44:12 +0900

    82までDone

--- app/src/main/java/com/example/koji_mitake/calculatetraining/TestActivity.kt
+++ app/src/main/java/com/example/koji_mitake/calculatetraining/TestActivity.kt
@@ -3,7 +3,10 @@ package com.example.koji_mitake.calculatetraining
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_test.*
+import java.util.*
 
 class TestActivity : AppCompatActivity (), View.OnClickListener {
 
@@ -17,7 +20,7 @@ class TestActivity : AppCompatActivity (), View.OnClickListener {
         val bundle : Bundle = intent.extras
         val selectedItem : Int = bundle.getInt("selectecItem")
 
-        //画面に表示
+        //画面に表示git push -u origin master
         textViewRemaining.text = selectedItem.toString()
 
         //クリックリスナーの登録
@@ -46,8 +49,34 @@ class TestActivity : AppCompatActivity (), View.OnClickListener {
         question()
 
     }
+    // 電卓が押された場合
+    override fun onClick(v : View?) {
 
-    override fun onClick(p0: View?) {
+        val button :Button = v as Button
+
+        when( v?.id ){
+            //クリアボタン
+            R.id.buttonClear
+                -> textViewAnswer.text = ""
+            //マイスの場合
+            R.id.buttonMinus
+                -> if(textViewAnswer.text.toString() == "") {
+                        textViewAnswer.text = "-"
+                    }
+            //０の場合
+            R.id.button0
+                    -> if (textViewAnswer.text.toString() != "0" &&
+                            textViewAnswer.text.toString() != "-" ){
+                            textViewAnswer.append(button.text)
+                        }
+            //その他の場合
+            else
+                if (textViewAnswer.text.toString() == "0")
+                    textViewAnswer.text = button.text
+                else textViewAnswer.append((button.text))
+
+        }
+
 
     }
 
@@ -57,6 +86,40 @@ class TestActivity : AppCompatActivity (), View.OnClickListener {
     }
 
     private fun question() {
+        //戻るボタンを使えなくする
+        buttonBack.isEnabled = false
+
+        //答え合わせボタンと電卓ボタンは使えるようにする
+        buttonAnswer.isEnabled = true
+        button0.isEnabled = true
+        button1.isEnabled = true
+        button2.isEnabled = true
+        button3.isEnabled = true
+        button4.isEnabled = true
+        button5.isEnabled = true
+        button6.isEnabled = true
+        button7.isEnabled = true
+        button8.isEnabled = true
+        button9.isEnabled = true
+
+        //問題の数字を1ｰ100からランダムに設定する
+        val random = Random()
+        val leftNumber = random.nextInt(100) + 1
+        val rightNumber = random.nextInt(100) + 1
+        textViewleft.text = leftNumber.toString()
+        textViewRight.text = rightNumber.toString()
+
+        // 計算方法を+ と − をランダムに表示
+        when(random.nextInt(2)){
+            1 -> textViewOperator.text = "+"
+            2 -> textViewOperator.text = "-"
+        }
+
+        //前の問題で入力した答えを消す
+        textViewAnswer.text = ""
+
+        // ◯ ✗ を見えないようにする
+        imageView.visibility = View.INVISIBLE
 
     }
 
